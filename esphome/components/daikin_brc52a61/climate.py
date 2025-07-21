c72df781a68649535651fd6210349209490df850
ilfuriano <ilfuriano@gmail.com>
2025-07-21 16:25:16 +0200

    Update climate.py

--- esphome/components/daikin_brc52a61/climate.py
+++ esphome/components/daikin_brc52a61/climate.py
@@ -8,13 +8,8 @@ AUTO_LOAD = ["climate_ir"]
 daikin_brc52a61_ns = cg.esphome_ns.namespace("daikin_brc52a61")
 DaikinClimate = daikin_brc52a61_ns.class_("DaikinClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(DaikinClimate),
-    }
-)
-
-
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(DaikinClimate)
+    
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await climate_ir.register_climate_ir(var, config)
